af07225ce01aac6280c1f1d9e2db19a0e3898a4b
MAKONJIO99 <francismakonjio2@gmail.com>
2021-06-24 01:57:56 +0300

    config.py change to production

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ class Config:
     """
     """
 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = '1999'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1999@localhost/pitch'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587 
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = 'francismakonjio2@gmail.com'
+    MAIL_PASSWORD = 'makonjio1999'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class ProdConfig(Config):
-    """
-    """
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1999@localhost/pitchup'
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
 class DevConfig(Config):
     """
     """
